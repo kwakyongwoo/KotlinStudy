04e23372855f4ee1096c0efb9e89bce9c4f5b187
2018007874 <kkolbuyw@gmail.com>
2020-02-04 16:32:34 +0900

    MainTodoViewHolder(draw a line through text in textView when checked)

--- app/src/main/java/com/example/kotlinstudy/main/MainTodoViewHolder.kt
+++ app/src/main/java/com/example/kotlinstudy/main/MainTodoViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinstudy.main
 
+import android.graphics.Paint
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinstudy.room.entitiy.TodoItem
@@ -7,7 +8,13 @@ import kotlinx.android.synthetic.main.item_todo.view.*
 
 class MainTodoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     fun onbind(item: TodoItem) {
-        itemView.todo_cb.isChecked = item.checked
+        itemView.todo_cb.isChecked = item.checked.also {
+            if(it) {
+                itemView.todo_tv_name.paintFlags = itemView.todo_tv_name.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            } else {
+                itemView.todo_tv_name.paintFlags = 0
+            }
+        }
         itemView.todo_tv_name.text = item.name
     }
 }
